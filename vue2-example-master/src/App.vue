91463ed5bced95b0c9551eb86e3d087b956dc57f
danial rajabi <44137507+danirajaba@users.noreply.github.com>
2018-10-14 18:12:56 +0330

    change name of links

--- vue2-example-master/src/App.vue
+++ vue2-example-master/src/App.vue
@@ -4,8 +4,8 @@
     <h1>Hello App!</h1>
     <firstcomponent></firstcomponent>
     <ul>
-        <li><router-link to="/first">点我跳转到第一页</router-link></li>
-        <li><router-link to="/second">点我跳转到第二页</router-link></li>
+        <li><router-link to="/first">Home</router-link></li>
+        <li><router-link to="/second">login</router-link></li>
       </ul>
     <router-view class="view"></router-view>
   </div>
